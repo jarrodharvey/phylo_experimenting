34c9199a9c8021926b0ed84a883340d48f08121a
Jarrod Harvey <41664216+jarrodharvey@users.noreply.github.com>
2022-06-10 00:17:41 +1000

    Add files via upload

---
+++ phylo_experimenting.R
@@ -0,0 +1,19 @@
+rm(list=setdiff(ls(), c("rD", "remDr")))
+cat("\014")
+
+easypackages::packages("dplyr", "rotl", "stringr")
+
+all_species <- read.csv("data/species_table.csv")
+
+random_species <- sample_n(all_species, sample(3:20, 1))
+
+original_phylo <- tol_induced_subtree(random_species$ott_id, label_format = "id")
+
+my_phylo <- original_phylo
+
+my_phylo$tip.label <- tibble(ott_id = str_remove(my_phylo$tip.label, "ott") %>%
+                               as.numeric(.)) %>%
+  left_join(., random_species) %>%
+  .$common.name
+
+plot(my_phylo)
